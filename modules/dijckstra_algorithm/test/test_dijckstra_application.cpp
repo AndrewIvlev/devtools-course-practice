676af37afb4a8b2d8200780b14bbd7673ddf4129
AndrewIvlev <mr.andrew.ivlev@gmail.com>
2019-10-18 21:28:16 +0300

    remove one test

--- modules/dijckstra_algorithm/test/test_dijckstra_application.cpp
+++ modules/dijckstra_algorithm/test/test_dijckstra_application.cpp
@@ -130,17 +130,6 @@ class DijckstraApplicationTest : public ::testing::Test {
     }
 };
 
-TEST_F(DijckstraApplicationTest, Shortest_path_between_two_vertex) {
-    // Arrange
-    DijckstraApplication calc;
-    std::string expected_result = "1 3 6 5";
-	std::vector<std::string> vec_arg = {"sp", "0", "5"};
-	
-	Act(vec_arg);
-
-    Assert(expected_result);
-}
-
 TEST_F(DijckstraApplicationTest, Validation_incorrect_input) {
     // Arrange
     DijckstraApplication calc;
